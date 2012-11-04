e6f57a652b3093c1476f10dcf5b5c8bfee716ca5
Stella Lie <ask@stellalie.com>
2012-11-04 21:54:29 +1100

    folders on EMR.java

--- EMR.java
+++ EMR.java
@@ -10,24 +10,24 @@ public class EMR {
              new File(args[3]));
    	 */
         Record record1 = new Record(
-            new File("records01.txt"), 
-            new File("instructions01.txt"), 
-            new File("out01.txt"),
-            new File("rep01.txt"));
+            new File("samples/records01.txt"), 
+            new File("samples/instructions01.txt"), 
+            new File("outputs/output01.txt"),
+            new File("outputs/report01.txt"));
         Record record2 = new Record(
-            new File("records02.txt"), 
-            new File("instructions02.txt"), 
-            new File("out02.txt"),
-            new File("rep02.txt")); 
+            new File("samples/records02.txt"), 
+            new File("samples/instructions02.txt"), 
+            new File("outputs/output02.txt"),
+            new File("outputs/report02.txt"));
         Record record3 = new Record(
-            new File("records03.txt"), 
-            new File("instructions03.txt"), 
-            new File("out03.txt"),
-            new File("rep03.txt"));
+            new File("samples/records03.txt"), 
+            new File("samples/instructions03.txt"), 
+            new File("outputs/output03.txt"),
+            new File("outputs/report03.txt"));
         Record record4 = new Record(
-            new File("records04.txt"), 
-            new File("instructions04.txt"), 
-            new File("out04.txt"),
-            new File("rep04.txt"));
+            new File("samples/records04.txt"), 
+            new File("samples/instructions04.txt"), 
+            new File("outputs/output04.txt"),
+            new File("outputs/report04.txt"));
     }
 }
