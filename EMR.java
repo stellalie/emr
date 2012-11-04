1ebcb6bbb8c76a88196dd161bd533e8344155249
Stella Lie <ask@stellalie.com>
2012-11-04 18:09:27 +1100

    auto load test files (no args)

--- EMR.java
+++ EMR.java
@@ -3,21 +3,31 @@ import java.io.*;
 public class EMR {  
     public static void main(String[] args) throws Exception {
    	 /*
+   	 Record record1 = new Record(
+             new File(args[0]), 
+             new File(args[1]), 
+             new File(args[2]),
+             new File(args[3]));
+   	 */
         Record record1 = new Record(
             new File("records01.txt"), 
             new File("instructions01.txt"), 
-            new File("out01.txt"));
+            new File("out01.txt"),
+            new File("rep01.txt"));
         Record record2 = new Record(
             new File("records02.txt"), 
             new File("instructions02.txt"), 
-            new File("out02.txt")); */
+            new File("out02.txt"),
+            new File("rep02.txt")); 
         Record record3 = new Record(
             new File("records03.txt"), 
             new File("instructions03.txt"), 
-            new File("out03.txt"));
+            new File("out03.txt"),
+            new File("rep03.txt"));
         Record record4 = new Record(
             new File("records04.txt"), 
             new File("instructions04.txt"), 
-            new File("out04.txt"));
+            new File("out04.txt"),
+            new File("rep04.txt"));
     }
 }
