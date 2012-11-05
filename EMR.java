4af8a6a6a162533d80655b9a1effc182fb46bdbc
Stella Lie <ask@stellalie.com>
2012-11-05 15:02:46 +1100

    tidying up

--- EMR.java
+++ EMR.java
@@ -9,25 +9,30 @@ public class EMR {
              new File(args[2]),
              new File(args[3]));
    	 */
-        Record record1 = new Record(
-            new File("samples/records01.txt"), 
-            new File("samples/instructions01.txt"), 
-            new File("outputs/output01.txt"),
-            new File("outputs/report01.txt"));
-        Record record2 = new Record(
-            new File("samples/records02.txt"), 
-            new File("samples/instructions02.txt"), 
-            new File("outputs/output02.txt"),
-            new File("outputs/report02.txt"));
-        Record record3 = new Record(
-            new File("samples/records03.txt"), 
-            new File("samples/instructions03.txt"), 
-            new File("outputs/output03.txt"),
-            new File("outputs/report03.txt"));
-        Record record4 = new Record(
-            new File("samples/records04.txt"), 
-            new File("samples/instructions04.txt"), 
-            new File("outputs/output04.txt"),
-            new File("outputs/report04.txt"));
+    Record record1 = new Record(
+        new File("samples/records01.txt"), 
+        new File("samples/instructions01.txt"), 
+        new File("outputs/output01.txt"),
+        new File("outputs/report01.txt"));
+    Record record2 = new Record(
+        new File("samples/records02.txt"), 
+        new File("samples/instructions02.txt"), 
+        new File("outputs/output02.txt"),
+        new File("outputs/report02.txt"));
+    Record record3 = new Record(
+        new File("samples/records03.txt"), 
+        new File("samples/instructions03.txt"), 
+        new File("outputs/output03.txt"),
+        new File("outputs/report03.txt"));
+    Record record4 = new Record(
+        new File("samples/records04.txt"), 
+        new File("samples/instructions04.txt"), 
+        new File("outputs/output04.txt"),
+        new File("outputs/report04.txt"));
+    Record record5 = new Record(
+        new File("samples/records05.txt"), 
+        new File("samples/instructions05.txt"), 
+        new File("outputs/output05.txt"),
+        new File("outputs/report05.txt"));
     }
 }
